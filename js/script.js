a2514a34a4dcbcf9795c9777440300d5fe2d0e17
Jing <adc1239@gmail.com>
2022-05-22 16:39:05 +0800

    收費方案

--- js/script.js
+++ js/script.js
@@ -1,6 +1,48 @@
 $(document).ready(function () {
 
+    $(window).on('load resize scroll', function () {
+        const window_top = $(this).scrollTop();
 
+        if (window_top > 60 && !$('body').hasClass('leavetop')) {
+            $('body').addClass('leavetop');
+        }
+
+        if (window_top < 60 && $('body').hasClass('leavetop')) {
+            $('body').removeClass('leavetop');
+        }
+    });
+
+    $('.gotop').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    if ($('.plan-select').length > 0) {
+        let nowPlan = 0;
+        function planSelect() {
+            $('.plan-select-nav').find('li').removeClass('active').eq(nowPlan).addClass('active');
+            $('.plan-select__content').find('.plan-select-item').hide().eq(nowPlan).fadeIn();
+        }
+        $(document).ready(planSelect);
+        $('.plan-select-nav>li').click(function(){
+            nowPlan = $('.plan-select-nav>li').index(this);
+            $(this).each(planSelect);
+        });
+    }
+
+    if ($('.faq').length > 0) {
+        let nowFaq = 0;
+        function viewFAQ() {
+            $('.faq-content').find('.faq-item').removeClass('active').eq(nowFaq).addClass('active');
+        }
+        $(document).ready(viewFAQ);
+        $('.faq-content>.faq-item').click(function () {
+            nowFaq = $('.faq-content>.faq-item').index(this);
+            $(this).each(viewFAQ);
+        });
+    }
 
     const swiper = new Swiper('.best-comment-swiper', {
         slidesPerView: 1,
@@ -26,25 +68,6 @@ $(document).ready(function () {
                 slidesPerView: 3,
             }
         }
-    });
-
-    $(window).on('load resize scroll', function () {
-        const window_top = $(this).scrollTop();
-
-        if (window_top > 60 && !$('body').hasClass('leavetop')) {
-            $('body').addClass('leavetop');
-        }
-
-        if (window_top < 60 && $('body').hasClass('leavetop')) {
-            $('body').removeClass('leavetop');
-        }
-    });
-
-    $('.gotop').on('click', function (event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
     });
 
 });
